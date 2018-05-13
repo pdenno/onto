8d7bdb345abe2f091c57ed65691f22b14e54ee4c
Peter Denno <podenno@gmail.com>
2018-05-13 14:14:36 -0400

    added mklinks.sh

---
+++ bin/mklinks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Do this in ./resources
+ln -s ../../ModelingOntology/SCHEMA_QUDT-v2.0.ttl
+ln -s ../../ModelingOntology/dtype.owl
+ln -s ../../ModelingOntology/modeling.ttl
+ln -s ../../ModelingOntology/operations.ttl
+ln -s ../../ModelingOntology/vaem.owl
