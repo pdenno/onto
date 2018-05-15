3bed7387cf5a1cc099eb00f01de0b3e1faf3708e
Peter Denno <podenno@gmail.com>
2018-05-15 17:32:09 -0400

    Now on the correct path

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure        "1.9.0"]
                  [org.clojure/core.logic     "0.8.11"] 
-               #_[uk.org.russet/tawny-owl   "1.6.0"]
                  [uk.org.russet/tawny-owl    "2.0.0-SNAPSHOT-pod"]]
-  :repl-options {:init-ns modelmeth.util})
+  :repl-options {:init-ns modelmeth.core})
                  
 
